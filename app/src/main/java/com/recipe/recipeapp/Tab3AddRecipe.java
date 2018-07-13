7aa6128afad291b66a1e93f56fa01384c576ab16
Donghyun <dhshk12@gmail.com>
2018-07-13 18:48:46 +0900

    update on the layout of add_recipe

--- app/src/main/java/com/recipe/recipeapp/Tab3AddRecipe.java
+++ app/src/main/java/com/recipe/recipeapp/Tab3AddRecipe.java
@@ -1,10 +1,12 @@
 package com.recipe.recipeapp;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.recipe.recipeapp.Objects.Recipe;
 
 public class Tab3AddRecipe extends Fragment {
 
@@ -14,4 +16,10 @@ public class Tab3AddRecipe extends Fragment {
         View rootView = inflater.inflate(R.layout.tab3_add_recipe, container, false);
         return rootView;
     }
+
+    TextInputEditText editTextName =(TextInputEditText)findViewById(R.id.editName);
+    editTextName.
+
+
+
 }
