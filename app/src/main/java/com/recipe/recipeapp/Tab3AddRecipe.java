464cc310b14c259c982fc32558d3c63de02e5dde
Donghyun <dhshk12@gmail.com>
2018-07-18 19:12:01 +0900

    added the spinner for cateogry in addRecipe

--- app/src/main/java/com/recipe/recipeapp/Tab3AddRecipe.java
+++ app/src/main/java/com/recipe/recipeapp/Tab3AddRecipe.java
@@ -1,26 +1,32 @@
 package com.recipe.recipeapp;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
-
 
 import com.recipe.recipeapp.Adapter.RecipeListAdapter;
 import com.recipe.recipeapp.Adapter.RecipeRecyclerAdapter;
 import com.recipe.recipeapp.Objects.Recipe;
 import com.recipe.recipeapp.Sample_Data.RecipeData;
+import com.recipe.recipeapp.Singleton.CategorySelectedSingleton;
+
+
 
 
 public class Tab3AddRecipe extends Fragment {
 
-    String name,description;
+    private String name,description,category;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +36,32 @@ public class Tab3AddRecipe extends Fragment {
 
         final EditText nameInput = (EditText) rootView.findViewById(R.id.editName);
         final EditText descriptionInput = (EditText) rootView.findViewById(R.id.editDescription);
+        final Spinner spinner = (Spinner) rootView.findViewById(R.id.spinner2);
+
+        ArrayAdapter<String> categoryListAdapter = new ArrayAdapter<>(this.getActivity(),
+                R.layout.custom_spinner_item,
+                getResources().getStringArray(R.array.categories_list));
+
+        categoryListAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(categoryListAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                CategorySelectedSingleton.getInstance().categorySelected =
+                        category=spinner.getSelectedItem().toString();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                CategorySelectedSingleton.getInstance().categorySelected = "All";
+            }
+        });
+
+
+        //when the add button is clicked, new recipe is added with the new information
+
+
         Button addButton = (Button) rootView.findViewById(R.id.add);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,7 +70,7 @@ public class Tab3AddRecipe extends Fragment {
                 description=descriptionInput.getText().toString();
 
                 Recipe newRecipe = (new Recipe(null, name,description,
-                        null, 2.5f, "noodle", null, null));
+                        null, 2.5f, category, null, null));
                 RecipeData recipeData = new RecipeData();
                 recipeData.addRecipe(newRecipe);
 
